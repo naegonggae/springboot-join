0c42a7ebeb56804652f0c008b1d0887d738afb23
naegonggae <https://github.com/naegonggae/>
2022-12-18 00:41:56 +0900

    project build 완료

---
+++ src/main/java/com/springboot/join/SpringbootJoinApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.join;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJoinApplication.class, args);
+    }
+
+}
